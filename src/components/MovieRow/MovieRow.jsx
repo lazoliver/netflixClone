6185aee75162433f65c6a60aa0e08b231a20a671
lazoliver <wilian.oliveiractt@gmail.com>
2022-01-30 13:06:33 -0300

    Buttons

--- src/components/MovieRow/MovieRow.jsx
+++ src/components/MovieRow/MovieRow.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import './MovieRow.css';
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 
 export default ({title, items}) => {
+    const [scrollX, setScrollX] = useState(-400);
+
+    const handleLeftArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        if(x > 0) {
+            x = 0;
+        }
+        setScrollX(x);
+    }
+
+    const handleRightArrow = () => {
+        let x = scrollX + Math.round(window.innerWidth / 2);
+        let listW = items.results.length * 150;
+        if((window.innerWidth - listW) > x) {
+            x = (window.innerWidth - listW) - 60;
+        }
+        setScrollX(x);
+    }
+
     return (
         <div className="movieRow">
             <h2>{title}</h2>
-            <div className="movieRow--listarea">
+            <div className="movieRow--left" onClick={handleLeftArrow}>
+                <NavigateBeforeIcon sx={{ my: 10 }} />
+            </div>
+            <div className="movieRow--right" onClick={handleRightArrow}>
+                <NavigateNextIcon sx={{ my: 10 }} />
+            </div>
+            <div className="movieRow--listarea" style={{
+                marginLeft: scrollX,
+                width: items.results.length * 152
+            }}>
                 <div className="movieRow--list">
                     {items.results.length > 0 && items.results.map((item, key) => (
                         <div key={key} className="movieRow--item">
